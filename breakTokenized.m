d74e60f178b64ce18816a2e883a23a5250fa7a73
xijial1113 <xijial@berkeley.edu>
2013-02-26 15:01:16 -0800

    first commit

---
+++ breakTokenized.m
@@ -0,0 +1,16 @@
+load('tokenized.mat');
+display('finished loading tokenized.mat')
+
+save('scnt.mat', 'scnt')
+display('finished saving scnt.mat')
+load('scnt.mat');
+%find the first element in scnt that is smaller than 500
+tmppos = find(scnt<=300,1, 'first');
+
+smap = smap(1:tmppos);
+save('smap.mat','smap')
+display('finished saving smap.mat')
+
+tokens =tokens(3,:);
+save('-v7.3', 'tokens.mat', 'tokens')
+display('finished saving tokens.mat')
